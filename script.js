9e75bb3f8a74d0a3adda0df91d1c3ff9a389511a
ttni131 <meteakdag5757@gmail.com>
2026-01-27 06:05:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// URL'de başarı mesajı varsa portalı aç
-window.onload = function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    if (urlParams.get('status') === 'success') {
-        hideAuth();
-    }
-}
-
-function hideAuth() {
-    document.getElementById('auth-screen').style.display = 'none';
-    document.getElementById('main-portal').style.display = 'block';
-}
-
 function showSection(id) {
     document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
     document.getElementById(id).classList.add('active');
@@ -23,15 +10,15 @@ function copyIp() {
     setTimeout(() => n.classList.remove('show'), 2000);
 }
 
-// Kar Efekti
 function createSnow() {
+    const snow = document.getElementById('snow');
     const snowflake = document.createElement('div');
     snowflake.classList.add('snowflake');
     snowflake.innerHTML = '❄';
     snowflake.style.left = Math.random() * 100 + 'vw';
     snowflake.style.animationDuration = Math.random() * 3 + 2 + 's';
     snowflake.style.opacity = Math.random();
-    document.getElementById('snow').appendChild(snowflake);
+    snow.appendChild(snowflake);
     setTimeout(() => snowflake.remove(), 5000);
 }
 setInterval(createSnow, 100);
